cc2acca3679d8d20bd5bcc333c08a999710a09bf
Xuhengfeng <564297479@qq.com>
2018-07-06 01:16:22 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
+/*
+ * @Author: 徐横峰 
+ * @Date: 2018-07-05 23:27:08 
+ * @Last Modified by:   mikey.zhaopeng 
+ * @Last Modified time: 2018-07-05 23:27:08 
+ */
 $(function(){
     //开启es5严格模式
     'use strict';
 
     // ol 列表动画
-    $('header nav li').hover(
+    $('#menu li').hover(
         function(){
             $(this).find('ol').show();
         },
@@ -13,66 +19,15 @@ $(function(){
     );
 
     // 城市modal 切换
-    $('.toggle').click(function(){
+    $('#toggleCity').click(function(){
         $('.citylist').fadeIn();
     })
-    $('.close').click(function(){
+    $('#closeDialog').click(function(){
         $('.citylist').fadeOut();
     })
-    
-
-    
-    //登录成功之后 append userInfo
-    var infoHtml = '<div class="user-login-after">'
-            +'<div class="headerImg">'
-                +'<img src="imgs/avatar.png" alt="头像">'                   
-                +'<span class="username">徐横峰</span>'
-                +'<span>/</span>'
-                +'<span class="user-register-btn user-logout">退出</span>'
-            +'</div>'
-        +'</div>'
-        +'<div class="user-login-list">'
-            +'<ul>'
-                +'<li><a href="#">消息</a></li>'
-                +'<li><a href="#">个人帐号</a></li>'
-                +'<li><a href="#">预约看房</a></li>'
-                +'<li><a href="#">我的收藏</a></li>'
-                +'<li><a href="#">我的委托</a></li>'
-            +'</ul>'
-        +'</div>'
-        
-    // 监听登录|登出按钮
-    $('.user-login')
-    .on('click', '.user-login-btn', function(){
-        $('.user-login').append(infoHtml);
-        $('.user-login .info').hide()
-    })
-    .on('click', '.user-logout',function(){
-        $('.user-login .info').show();
-        $('.user-login-after').hide()
-    })
-    // 监听用户昵称
-    .on('mouseover', '.username', function(){
-        $('.user-login-list').show();
-    })
-    .on('mouseout','.username', function(){
-        $('.user-login-list').hide();        
-    })
-    // 监听用户登录之后的list
-    .on('mouseover','.user-login-list', function(){
-        $(this).show();
-    })
-    .on('mouseout','.user-login-list', function(){
-        $(this).hide();
-    })
-
-
-
-
-
-
+      
     //检索 二手房 租房  小区
-    $('.search ul>li').click(function(){
+    $('#selectQuery li').click(function(){
         var num = $(this).index();
         switch(num) {
             case 0: 
@@ -90,8 +45,4 @@ $(function(){
         }
     })
 
-    //登录|注册
-    $('.loginContent .close').click(function(){
-        $('.loginContent').hide();
-    })
 })
---
+++ js/login.js
@@ -0,0 +1,65 @@
+/*
+ * @Author: 徐横峰 
+ * @Date: 2018-07-05 23:27:02 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-07-05 23:28:47
+ */
+$(function(){
+    //开启es5严格模式
+    'use strict';
+    
+    //登录成功之后 append userInfo
+    var infoHtml = '<div class="user-login-after">'
+            +'<div class="headerImg">'
+                +'<img src="imgs/avatar.png" alt="头像">'                   
+                +'<span class="username">徐横峰</span>'
+                +'<span>/</span>'
+                +'<span class="user-register-btn user-logout">退出</span>'
+            +'</div>'
+        +'</div>'
+        +'<div class="user-login-list">'
+            +'<ul>'
+                +'<li><a href="#">消息</a></li>'
+                +'<li><a href="#">个人帐号</a></li>'
+                +'<li><a href="#">预约看房</a></li>'
+                +'<li><a href="#">我的收藏</a></li>'
+                +'<li><a href="#">我的委托</a></li>'
+            +'</ul>'
+        +'</div>'
+        
+    // 监听用户登录|登出操作
+    $('#user')
+    .on('click', '.user-login-btn', function(){
+        // 打开登录面板
+        $('#loginContentBox').show();
+        
+        // 添加登录信息
+        $('.user-login').append(infoHtml);
+        $('.user-login .info').hide()
+    })
+    .on('click', '.user-logout',function(){
+        $('.user-login .info').show();
+        // 移除登录信息
+        $('.user-login-after').remove();
+        $('.user-login-list').remove();
+    })
+    // 监听用户昵称
+    .on('mouseover', '.username', function(){
+        $('.user-login-list').show();
+    })
+    .on('mouseout','.username', function(){
+        $('.user-login-list').hide();        
+    })
+    // 监听用户登录之后的list
+    .on('mouseover','.user-login-list', function(){
+        $(this).show();
+    })
+    .on('mouseout','.user-login-list', function(){
+        $(this).hide();
+    })
+
+    //登录|注册
+    $('#loginContentBox .close').click(function(){
+        $('#loginContentBox').hide();
+    })
+})
