504e18ebfa546835cedaad1643825460fca7c51b
xuhengfeng <564297479@qq.com>
2018-08-22 20:47:54 +0800

    update

--- js/chat.js
+++ js/chat.js
@@ -1,61 +1,442 @@
 'use strict';
- // 挂载IM
- window.JIM = new JMessage({debug: true});
+var userInfo;            //登录用户会员信息
+var userbrokerList;      //登录用户的会话列表
+var chatMe;              //登录的用户唯一标识
+var chatUsername;        //经纪人唯一标识
+var showChatlist = true; //用来切换聊天窗口
+var showChatBox = false; //用来记住聊天区域是否被打开
+var currentChatObj = {}; //当前聊天的对象
+var photo;               //经纪人头像
+try{                     //如果用户已经登录
+	//刷新当前的会话列表
+	brokerAllList();
+}catch(error){};
 
- //初始化极光IM
-function Jiguang_Init() {
-    JIM.init({
-        appkey: this.AuthJiG.appkey,
-        random_str: this.AuthJiG.random_str,
-        signature: this.AuthJiG.signature,
-        timestamp: this.AuthJiG.timestamp,
-        flag: 1,
-    })
-    .onSuccess(data => {
-        Jiguang_login();//极光登录
-    })
-    .onFail(error => {});
+// 监听聊天窗口关闭
+$('.closeChat').click(function(){
+	chatBox_close();
+	showChatBox = false;
+})
+
+// 监听聊天窗口打开
+$('.chatList')
+.on('click', '.title', function() {
+	//判断是否登录
+	if(sessionStorage.userInfo){
+		//切换聊天窗口关开
+		if(showChatlist){
+			chatBox_open();
+			showChatlist = false;
+			//判断聊天区域是否显示
+			if(showChatBox==false){
+				$('.chatBox').hide();
+			}
+		}else{
+			chatBox_close();
+			showChatlist = true;
+		}			
+	}else{
+		loginBox_open();
+	}
+})
+.on('click', 'li', function() {
+	var id = $(this).data().id, ret;	
+		ret =  userbrokerList.find(function(item){
+			return item.chatUsername === id;
+		})		
+	//刷新当前的聊天经纪人
+	currentChatObj = Object.assign({}, ret);
+	$('#currentBroker').text(currentChatObj.emplName);
+	chatBox_open();	
+	showChatBox = true;
+	//刷新当前的聊天的内容
+	$('#chatContent>li').remove();
+	refreshChat(ret);
+	//回到底部
+	$('.scroll').get(0).scrollIntoView(false);
+})
+
+
+// 联系经纪人
+$('.linkBroker').click(function(e){
+	var emplName = $(this).attr('emplName');
+	var phone = $(this).attr('phone');
+	chatUsername = $(this).attr('chatusername');
+	photo = $(this).attr('photo')||'/custAppApi/imgs/avatar.png';	
+	//判断是否登录
+	if (sessionStorage.userInfo) {
+		userInfo = JSON.parse(sessionStorage.userInfo);
+		chatMe =  userInfo.easemobUsername;
+		//打开聊天窗口
+		chatBox_open();
+		//构建经纪人对象
+		var currentObj = {
+			emplName: emplName,
+			chatUsername: chatUsername,
+			phone: phone,
+			photo: photo,
+			history: [],//历史聊天记录
+			ext: {},
+			weichat: {originType: "webim"}
+		}
+		//拷贝当前的聊天对象
+		currentChatObj =  Object.assign({}, currentObj);
+		//判断缓存中是否存在
+		if(localStorage.getItem(chatMe)){
+			var list = JSON.parse(localStorage.getItem(chatMe));
+			var ret =  list.find(function(item){
+				return item.chatUsername === chatUsername;
+			})	
+			//缓存中不存在 则添加进去
+			if(ret==null||ret==undefined){			
+				list.push(currentObj);
+				//刷新当前的会话列表
+				brokerList(currentObj);
+				localStorage.setItem(chatMe, JSON.stringify(list));
+			}else{
+				//刷新当前的聊天内容
+				refreshChat(ret)
+			}
+			//刷新当前的聊天经纪人
+			$('#currentBroker').text(currentObj.emplName);
+		}else{
+			var list = [];
+			list.push(currentObj);
+			list.forEach(function(item){brokerList(item)});
+			//刷新会话列表
+			brokerAllList();
+			localStorage.setItem(chatMe, JSON.stringify(list));
+		}
+		//回到底部
+		setTimeout(function(){$('.scroll').get(0).scrollIntoView(false)},200);
+	} else {
+		// 未登录则打开登录
+		loginBox_open();
+	}
+})
+
+//刷新整个会话列表
+function brokerAllList() {
+	userInfo = JSON.parse(sessionStorage.userInfo);
+	chatMe =  userInfo.easemobUsername;
+	userbrokerList = JSON.parse(localStorage.getItem(chatMe));
+	try{
+		userbrokerList.forEach(function(item){
+			brokerList(item);
+		})		
+	}catch(error){}
+	$('.noBrokers').hide();
 }
 
-//极光登录
-function Jiguang_login() {
-    JIM.login({
-      username: this.userInfo.easemobUsername,
-          password: this.userInfo.easemobPassword
-      })
-      .onSuccess(data => {
-          this.Jiguang_onDisconnect();//监听是否在线
-          this.Jiguang_userInfo();//用户信息
-          this.Jiguang_conversation();//这里首次会话列表
-          this.Jiguang_syncConversation();//这里首次同步监听离线消息
-          this.$refs.oChat.Jiguang_onMsg();//监听消息
-      })
-      .onFail(data => {});
+//添加经纪人到会话列表的UI上
+function brokerList(item) {
+	if(item){		
+		var lastChat,lastMsg,dataTime,changeHtml;
+		try{
+			lastChat = item.history.length?item.history[item.history.length-1]:null;//该对象的最后一条聊天map
+			lastMsg = lastChat!=null?lastChat.body.msg:null;//创建最后一条聊天记录
+			dataTime = lastChat!=null?newDate(lastChat.dataTime):'';//创建聊天时间
+			changeHtml = lastMsg==null?(''):('<p>'+lastMsg+'</p>');//判断是否存在最后一条消息
+		}catch(error){};
+		var img = item.photo?item.photo:'/custAppApi/imgs/avatar.png';//创建经纪人的头像
+		$('#chatBorkerList').append(						
+			'<li data-id="'+ item.chatUsername +'"><div class="time">'+dataTime+'</div>'+
+				'<div class="broker">'+
+					'<div class="fl img"><img src="'+img+'"/></div>'+
+					'<div class="fl orow">'+
+						'<h4>'+item.emplName+'</h4>' + changeHtml +
+					'</div>'+
+				'</div>'+
+			'</li>'
+		)
+		$('.noBrokers').hide();
+	}
 }
 
-//监听是否在线
-function Jiguang_onDisconnect() {
-    JIM.onDisconnect(res=>{
-      this.afresh();
-    })
+//添加聊天内容到显示聊天内容的UI上
+function appendChat(obj) {
+	if(obj){	
+		var lastChat = obj.history[obj.history.length-1];//该对象的最后一条聊天对象
+		var lastMsg = lastChat.body.msg||'';//创建聊天数据
+		var dataTime = newDate(lastChat.dataTime);//创建聊天时间
+		var img;//头像 
+		var className;//切换类 
+		var changeHtml;//文本或者图片结构
+		//判断是否是主动发送
+		if(obj.to != userInfo.easemobUsername){
+			className = 'chat-block-right';
+			img = JSON.parse(sessionStorage.userInfo).headImage||'/custAppApi/imgs/avatar.png';//创建自己的头像
+		}else{
+			className = 'chat-block-left';
+			img = obj.photo;//创建经纪人头像
+		}
+		try{
+			changeHtml = lastChat.type=='img'?('<img src="'+lastChat.body.url+'"/>'):('<div>'+lastMsg+'</div></div>');	
+		}catch(e){}
+		$('#chatContent .scroll').before(						
+			'<li>'+
+			'<div class="'+className+'">'+
+			'<div class="chat-time">'+dataTime+'</div>'+
+			'<div class="chat-block">'+
+			'<a href="#me"><img src="'+img+'"/></a>'+
+			'<div class="chat-content">'+changeHtml+'</div>'+
+			'</div>'+
+			'</li>'
+		);
+	}
 }
 
-//获取极光IM会话列表
-function Jiguang_conversation() {
-    JIM.getConversation()
-    .onSuccess(data => {
-        //缓存好友列表
-        localStorage.firend = data.conversations;
-    })
-    .onFail(data => {});
+//切换经纪人聊天
+function refreshChat(ret) {
+	if(ret){
+		//判断聊天记录
+		ret.history.forEach(function(val){
+			var className;//切换类 
+			var img;//头像 
+			var changeHtml;//文本或者图片结构
+			var dataTime = newDate(val.dataTime);//创建聊天时间
+			var msg = val.body.msg||'';//创建聊天数据
+			if(val.to != userInfo.easemobUsername){
+				className = 'chat-block-right';
+				img = JSON.parse(sessionStorage.userInfo).headImage||'/custAppApi/imgs/avatar.png';//创建自己的头像
+			}else{
+				className = 'chat-block-left';
+				img = ret.photo;//创建经纪人头像
+			}
+			try{
+				changeHtml = val.type=='img'?('<img src="'+val.body.url+'"/>'):('<div>'+msg+'</div></div>');	
+			}catch(e){}
+			$('#chatContent .scroll').before(						
+				'<li>'+
+				'<div class="'+className+'">'+
+				'<div class="chat-time">'+dataTime+'</div>'+
+				'<div class="chat-block">'+
+				'<a href="#me"><img src="'+img+'"/></a>'+
+				'<div class="chat-content">'+changeHtml+'</div>'+
+				'</div>'+
+				'</li>'
+			);
+		})
+	}
 }
 
-//离线消息同步监听
-function Jiguang_syncConversation(){
-    JIM.onSyncConversation(data=> {
-        //缓存历史漫游消息
-        localStorage.history = data;
-    });
+
+//聊天
+function weiChat() {	
+	//刷新整个会话列表
+	brokerAllList();
+	// 配置环信
+	var options = {
+		xmppURL : WebIM.config.xmppURL,
+		apiUrl : WebIM.config.apiURL,
+		appKey : '1163180517099289#dichan-app-test',
+		user : userInfo.easemobUsername,
+		pwd : userInfo.easemobPassword
+	};
+	window.conn = new WebIM.connection({
+		isMultiLoginSessions : WebIM.config.isMultiLoginSessions,
+		https : typeof WebIM.config.https === 'boolean' ? WebIM.config.https
+				: location.protocol === 'https:',
+		url : WebIM.config.xmppURL,
+		heartBeatWait : WebIM.config.heartBeatWait,
+		autoReconnectNumMax : WebIM.config.autoReconnectNumMax,
+		autoReconnectInterval : WebIM.config.autoReconnectInterval,
+		apiUrl : WebIM.config.apiURL,
+		isAutoLogin : true
+	});
+	//打开连接
+	conn.open(options);		
+	// 事件监听
+	conn.listen({
+		onOpened : function(message) { // 连接成功回调，连接成功后才可以发送消息
+			console.log("%c [opened] 连接已成功建立", "color: green")
+		},
+		onTextMessage : function(message) {// 收到文本消息
+			//判断哪个经纪人发来的消息
+			var list = JSON.parse(localStorage.getItem(chatMe));
+			var ret =  list.find(function(item){
+				return item.chatUsername === message.chatMe;
+			})	
+			$('#chatBorkerList>li').each(function(index,item){
+				if($(item).data('id')==ret.chatUsername){
+					$(item).addClass('unread');
+				}
+			})
+			
+			var msg = {
+				body: {
+					body: {type: 'txt', msg: message.data},
+					type: 'txt',
+					to: message.to
+				}	
+			}
+			
+			//当前时间搓
+			var currentTime = new Date().getTime();
+			//刷新聊天内容到显示聊天内容的UI上
+			refreshContent(msg, currentTime);
+			//回到底部
+			$('.scroll').get(0).scrollIntoView(false);
+
+			//判断有未读消息且聊天窗口未打开
+			var timer = setInterval(function(){
+				$('#pulse').css({background: '#fff'});
+				setTimeout(function(){
+					$('#pulse').css({background: "url('/custAppApi/imgs/shihua.png') no-repeat 12px 12px"});						
+					showChatlist&&clearInterval(timer);
+				},100);
+			}, 800)
+			
+			
+		}, 
+		onPictureMessage : function(message) { //收到图片消息
+			var options = {url : message.url};
+			options.onFileDownloadComplete = function() {
+				// 图片下载成功
+				console.log('Image download complete!');
+			};
+			options.onFileDownloadError = function() {
+				// 图片下载失败
+				console.log('Image download failed!');
+			};
+			WebIM.utils.download.call(conn, options); // 意义待查		
+			
+			var msg = {
+				body: {
+					body: {
+						type: 'img', 
+						msg: message.data,
+						url: message.url
+					},
+					type: 'img',
+					to: message.to
+				}	
+			}
+		
+			//当前时间搓
+			var currentTime = new Date().getTime();
+			//刷新聊天内容到显示聊天内容的UI上
+			refreshContent(msg, currentTime);
+			//回到底部
+			$('.scroll').get(0).scrollIntoView(false)
+
+		},
+		onError : function(message) {// 本机网络掉线
+			if (message && message.type == 1) {
+				console.warn('连接建立失败！请确认您的登录账号是否和appKey匹配。')
+			}
+		}
+	});
 }
+
+//刷新实时聊天内容到显示聊天内容的UI上
+function refreshContent(msg, currentTime) {
+	msg.body.dataTime = currentTime;
+	currentChatObj.history.push(msg.body);
+	//刷新实时聊天内容
+	appendChat(currentChatObj);
+	//重新缓存
+	var list = JSON.parse(localStorage.getItem(chatMe));
+	var query =  list.findIndex(function(item){
+		return item.chatUsername === currentChatObj.chatUsername;
+	});
+	list[query]=currentChatObj;
+	localStorage.setItem(chatMe, JSON.stringify(list));
+
+}
+
+
+// 私聊发送文本消息
+function sendPrivateText(msgData) {
+	console.log(currentChatObj)
+	
+	var id = conn.getUniqueId();
+	var msg = new WebIM.message('txt', id);
+	var currentTime = new Date().getTime();//当前时间搓
+	msg.set({
+		msg : msgData,
+		to: chatUsername,
+		roomType : false,
+		newTime: currentTime,
+		ext: {
+			nickName: currentChatObj.emplName,
+			headImg: currentChatObj.photo
+		},
+		success : function(id, serverMsgId) {
+			//刷新聊天内容到显示聊天内容的UI上
+			refreshContent(msg, currentTime);
+			//回到底部
+			$('.scroll').get(0).scrollIntoView(false);
+		}
+	});
+	msg.body.chatType = 'singleChat';
+	window.conn.send(msg.body);
+};	
+// 私聊发送图片消息
+function sendPrivateImg() {
+    var id = window.conn.getUniqueId();
+    var msg = new WebIM.message('img', id);
+    var input = document.getElementById('sendImg');               // 选择图片的input
+    var file = WebIM.utils.getFileUrl(input);// 将图片转化为二进制文件
+    var currentTime = new Date().getTime();//当前时间搓
+    var allowType = {
+        'jpg': true,
+        'gif': true,
+        'png': true,
+        'bmp': true
+    };
+    var option = {
+        apiUrl: WebIM.config.apiURL,
+        file: file,
+		to: chatUsername,
+        roomType: false,
+        chatType: 'img',
+        ext: {
+			nickName: currentChatObj.emplName,
+			headImg: currentChatObj.photo
+		},
+        onFileUploadError: function () {
+            console.log('onFileUploadError');
+        },
+        onFileUploadComplete: function () {
+        	//刷新聊天内容到显示聊天内容的UI上
+			refreshContent(msg, currentTime);
+		},
+		success: function(){
+			//回到底部
+			setTimeout(function(){$('.scroll').get(0).scrollIntoView(false)},300);
+		}
+    };
+    // forie8
+    try {
+        if (!file.filetype.toLowerCase() in allowType) {
+            console.log('file type error')
+            return
+        }
+    } catch (e) {
+        option.flashUpload = WebIM.flashUpload
+    }
+    msg.set(option);
+    window.conn.send(msg.body);
+};
+// 发送图片
+$('#sendImg').change(function(){
+	sendPrivateImg();
+})
+// 发送消息
+$('.sendBtn').click(function() {
+	var $sendText = $('textarea').val();
+	sendPrivateText($sendText);
+	$('textarea').val('');
+})
+// 按下回车
+$("textarea").keydown(function(e){
+	var ev = window.event ? window.event : e;
+    if(ev.keyCode==13) {
+    	var $sendText = $('textarea').val();
+    	sendPrivateText($sendText);
+        $(this).val('').focus(); 
+     }
+})
 
 
--- js/twohousedetail.js
+++ js/twohousedetail.js
@@ -1,150 +1,334 @@
+'use strict';
 /*
  * @Author: 徐横峰 
  * @Date: 2018-07-08 01:32:30 
- * @Last Modified by: Xuhengfeng
- * @Last Modified time: 2018-07-26 01:06:47
+ * @Last Modified by: 564297479@qq.com
+ * @Last Modified time: 2018-08-22 20:47:43
  */
 
-$(function(){
-    //开启es5严格模式
-    'use strict';
+//百度地图实例化
+var cityName = '北海';
+var omap = new BMap.Map("oMap");
+var point = new BMap.Point(info.px, info.py);//城市的坐标
+var marker = new BMap.Marker(point);//将点转化成标注点
+omap.centerAndZoom(point, 12);//初始中心和倍数
+omap.enableScrollWheelZoom(true);// 开启滚轮放大缩小
+omap.addOverlay(marker);  //将标注点添加到地图上
+var infoWindow = new BMap.InfoWindow(info.houseTitle, {// 创建信息窗口对象 
+    width : 20,    // 信息窗口宽度    
+    height: 5,     // 信息窗口高度    
+    title: ''      // 信息窗口标题   
+});     
+omap.openInfoWindow(infoWindow, point); 
 
-    // ol 列表动画
-    $('#menu li').hover(
-        function(){
-            $(this).find('ol').show();
-        },
-        function(){
-            $(this).find('ol').hide();
-        }
-    );
-    
-    // 图片交互区域
-    // 实例1
-    var oSwiper1 = new Swiper(".view .swiper-container", {
-                        onSlideChangeStart: function() {
-                            updateNavPosition();
-                        }
-                    });
-    // 实例2
-    var oSwiper2 = new Swiper(".preview .swiper-container", {
-                        visibilityFullFit: true,
-                        slidesPerView: "auto",
-                        allowTouchMove: false,
-                        onTap: function() {
-                            oSwiper1.slideTo(oSwiper2.clickedIndex);
-                        }
-                    });
-                    
-      // 上一张图片
-      $(".view .arrow-left,.preview .arrow-left")
-      .on("click", function(e) {
-            e.preventDefault();
-            if (oSwiper1.activeIndex == 0) {
-                oSwiper1.slideTo(oSwiper1.slides.length - 1, 1000);
-            return;
-            }
-            oSwiper1.slidePrev();
-      });
-
-      // 下一张图片
-      $(".view .arrow-right,.preview .arrow-right")
-      .on("click", function(e) {
-            e.preventDefault();
-            if (oSwiper1.activeIndex == oSwiper1.slides.length - 1) {
-            oSwiper1.slideTo(0, 1000);
-            return;
-            }
-            oSwiper1.slideNext();
-      });
-
-     
-      // 更新acitve位置
-      function updateNavPosition() {
-          $(".preview .active-nav").removeClass("active-nav");
-          var activeNav = $(".preview .swiper-slide")
-              .eq(oSwiper1.activeIndex)
-              .addClass("active-nav");
-          if (!activeNav.hasClass("swiper-slide-visible")) {
-              if (activeNav.index() > oSwiper2.activeIndex) 
-              {
-                var thumbsPerNav = Math.floor(oSwiper2.width / activeNav.width()) - 1;
-                    oSwiper2.slideTo(activeNav.index() - thumbsPerNav);
-              } 
-              else 
-              {
-                oSwiper2.slideTo(activeNav.index());
-              }
-          }
-      }
-      
-      // 百度地图实例化
-      var cityName = '北海';
-      var omap = new BMap.Map("oMap");
-      var point = new BMap.Point(116.331398, 39.897445);//城市的坐标
-      omap.centerAndZoom(point, 12);
-      omap.enableScrollWheelZoom(true);//开启滚轮放大缩小
-      
-      // 百度地图检索实例化
-      function selectMap(num) {
-          console.log(num)
-          var local = new BMap.LocalSearch(omap, 
-            {
-                renderOptions: {map: omap, autoViewport: true}
-            }
-        );
-        switch(num) {
-            case 0: return local.searchNearby('交通', cityName);
-            case 1: return local.searchNearby('购物', cityName);
-            case 2: return local.searchNearby('学校', cityName);
-            case 3: return local.searchNearby('餐饮', cityName);
-            case 4: return local.searchNearby('医疗', cityName);
-            case 5: return local.searchNearby('娱乐', cityName);
-        }
-        
-    }
-    
-    // 监听房源位置tab点击
-    selectMap(0);//初始回调一次
-    $('.queryword').on('click', 'ul>li', function(){
-        var num = $(this).index();
-        selectMap(num);
-    })
-    
-    //放大镜
-    $('.view')
-    .mousemove(function(ent){
-        $('#move,#bigImg').show();
-        var e = ent||event;
-        //文档位置
-        var x = e.pageX;
-        var y = e.pageY;
-        //坐标原点对齐
-        x = x - $(this).offset().left;
-        y = y - $(this).offset().top;
-        //move的位置
-        x = x - $('#move').width()/2;
-        y = y - $('#move').height()/2;
-        //边界限制
-        var maxLeft = $(this).width()-$('#move').width();
-        var maxTop = $(this).height()-$('#move').height();
-        if(x<=0){
-            x = 0;
-        }else if(x>=maxLeft){
-            x = maxLeft;
-        }
-        if(y<=0){
-            y = 0;
-        }else if(y>=maxTop){
-            y = maxTop;
-        }
-        //移动的比例
-		var bLeft = x / maxLeft * ($('#bigImg img').width()-$('#bigImg').width());
-        var bTop =  y / maxTop * ($('#bigImg img').height()-$('#bigImg').height());
-        $('#move').css({'left': x, 'top': y});
-        $('#bigImg img').css({'marginLeft': -bLeft, 'marginTop': -bTop});
-    })
-    .mouseout(function(){
-        $('#move,#bigImg').hide();
-    })
+//百度地图检索实例化
+function selectMap(num) {
+	var local = new BMap.LocalSearch(omap, {
+		renderOptions : {
+			map : omap,
+			autoViewport : true
+		}
+	});
+	switch (num) {
+	case 0:
+		return local.searchNearby('交通', cityName);
+	case 1:
+		return local.searchNearby('购物', cityName);
+	case 2:
+		return local.searchNearby('学校', cityName);
+	case 3:
+		return local.searchNearby('餐饮', cityName);
+	case 4:
+		return local.searchNearby('医疗', cityName);
+	case 5:
+		return local.searchNearby('娱乐', cityName);
+	}
+}
+$('.queryword').on('click', 'ul>li', function() {
+	var num = $(this).index();
+	selectMap(num);
 })
+
+//带看记录 
+$('#next,#prev').click(function(){
+	if($(this).find('.xhf-icon-left').length){
+		page--;
+		if(page<=1){
+			page=1;
+		}
+	}else{
+		page++;
+	}
+	var params = {
+		scity: 'beihai',
+		pageNo: page,
+		id: info.id
+	}
+	var result = api_twoHouseSeeHouseList(params);
+	var $td1 = $('.houseDynamic .two tr:first-child');
+	var $td2 = $('.houseDynamic .two tr:not(":first-child")');
+	//先进行清空
+	$td2.remove();
+	result.data.forEach(function(item,index){
+		$td1.after(		
+			'<tr class="tr-desc">'+
+			'<td>'+item.seeDate+'</td>'+
+			'<td>'+item.emplName+'</td>'+
+			'<td>'+item.phone+'</td>'+
+			'</tr>' 
+		)
+	}); 
+})
+
+//登录之后获取数据
+function loginAfterRefresh() {
+	try{var scity = JSON.parse(localStorage.selectCity).scity||'beihai'}catch(error){};
+	if(sessionStorage.userInfo){
+		var params = {
+			scity: scity,
+			sdid: info.sdid,
+			houseType: 'HOUSE' 
+		};
+		var result =  api_loginCorrelation(params).data;
+		//判断是否预约
+		result.isAppoint? $('#appoint').text('已预约') : $('#appoint').text('预约看房');
+		//判断是否收藏
+		result.isCollect? $('#addCollection').text('已收藏') : $('#addCollection').text('收藏房源');	
+		//判断是否加入
+		result.isComparison ? $('#comparison>span').text('已加入') : $('#comparison>span').text('加入对比');
+		
+		
+		
+//			$('#comparison>span').text('加入对比');
+		//预约看房列表
+		appendAppointHtml(result.houseList);	
+		//对比列表
+		appendContrastHtml(result.contrasList);
+	}
+}
+
+
+// console.log('------------------------------------------------预约看房清单操作-----------------------------------------------------------')
+
+//点击预约看房
+$('#appoint').click(function(e){
+	if($(this).text()=='已预约')return layer.msg('已存在约看房源列表');
+	var src = $('.view .swiper-slide-active img').attr('src');
+	var params = {scity: info.scity,sdid: info.sdid};
+	//判断是否加入预约看房
+	var result = api_appointMent(params);
+	if(result.status == '1'){
+		$(this).text('已预约');//加入预约成功		
+		addProduct(e, '.end1', src);//飞入动画
+		appendAppointHtml();//刷新预约看房列表
+	}else{
+		layer.msg(result.msg);
+	}
+})
+
+//预约看房列表
+function appendAppointHtml() {
+	var params = {scity: info.scity,pageNo: 1,pageSize: 4};
+	var result = api_appointHouseList(params);
+	//数据长度存在
+	if(result!=undefined&&result.length){
+		//先清空列表再进行刷新
+		$('#appointList>li:not(:last-child)').remove();
+		//显示出按钮
+		$('#appointBtn').show().siblings().hide();		
+		//刷新列表
+		result.forEach(function(item){
+			$('#appointList>li:last-child').before(
+				'<li data-scity="'+item.scity+'" data-id="'+item.id+'">'+
+					'<div class="fl image">'+
+						'<img src="'+item.housePic+'" alt="图片" />'+
+					'</div>'+
+					'<div class="r-content">'+
+						'<div>'+item.houseTitle+'</div>'+
+					'<div>'+
+					'	<span>'+item.houseType+'</span><span>'+item.builtArea+'平</span>'+
+					'</div>'+
+					'<div>'+
+						'<span>'+item.saleTotal+'</span>万'+
+					'</div>'+
+					'</div>'+
+					'<div class="delete">删除</div>'+
+				'</li>'
+			)
+		})
+	}else{
+		//隐藏按钮
+		$('#appointBtn').hide().siblings().show();	
+	}
+}
+
+//监听预约看房删除
+$('#appointList').on('click', '.delete', function(){
+	var params = {
+		scity: info.scity,
+		id: $(this).parent().data().id
+	}
+	var result = api_appointDeleteOne(params);
+	//刷新状态
+	loginAfterRefresh();
+})
+
+
+
+
+// console.log('------------------------------------------------对比清单操作-----------------------------------------------------------')
+
+//点击加入对比
+$('#comparison').click(function(e){
+	//判断是否预约
+	if ($('#comparison>span').text()=='已加入') return layer.msg('已存在对比房源列表中');
+	var src = $('.view .swiper-slide-active img').attr('src');
+	var params = {scity: info.scity,houseId: info.id, houseSdid: info.sdid};
+	var result = api_twohouseContrast(params);
+	//判断是否加入对比
+	if(result.status == '1'){
+		$('#comparison>span').text('已加入');//加入对比成功
+		addProduct(e, '.end2', src);//飞入动画
+		appendContrastHtml();//刷新对比列表
+	}else{
+		layer.msg(result.msg);
+	}
+})
+
+
+//生成对比列表结构
+function appendContrastHtml(result) {	
+	var params = {scity: info.scity, pageNo: 1, pageSize: 4};
+	//如果为空再去另外一个接口查看
+	var result = result!=null? result : api_twohouseList(params).data;
+	if(result.length){
+		//先清空
+		$('#contrastList>li:not(:last-child)').remove();
+		//显示出按钮
+		$('#contrastBtn').show().siblings().hide();
+		//刷新列表
+		result.forEach(function(item,index){
+			$('#contrastList>li:last-child').before(
+				'<li data-scity="'+item.scity+'" data-sdid="'+item.sdid+'">'+
+					'<div class="fl image">'+
+						'<img src="'+item.housePic+'" alt="图片" />'+
+					'</div>'+
+					'<div class="r-content">'+
+						'<div>'+item.houseTitle+'</div>'+
+					'<div>'+
+					'	<span>'+item.houseType+'</span><span>'+item.builtArea+'平</span>'+
+					'</div>'+
+					'<div>'+
+						'<span>'+item.saleTotal+'</span>万'+
+					'</div>'+
+					'</div>'+
+					'<div class="delete">删除</div>'+
+				'</li>'
+			)
+		})
+	}else{
+		//隐藏按钮
+		$('#contrastBtn').hide().siblings().show();
+	}
+}
+
+//对比房源清空全部
+$('#clearAllContrastList').click(function(){
+	var list = $('#contrastList>li:not(:last-child)');
+	list.each(function(index,item){
+		//请求删除
+		var params = {sdid: $(item).data().sdid, scity: $(item).data().scity};
+		var result = api_twohouseListDel(params);
+	})
+	//清空还原
+	$('#contrastList>li:not(:last-child)').remove();
+	$('#contrastBtn').hide().siblings().show();
+	//刷新状态
+	loginAfterRefresh();
+})
+
+//对比房源单删
+$('#contrastList').on('click', '.delete', function(){
+	var data = $(this).parent().data();
+	var list = $('#contrastList>li');
+	var findIndex;
+	list.each(function(index,item){
+		if($(item).data().sdid==data.sdid){
+			return findIndex = index;
+		}
+	})
+	//请求删除
+	var params = {sdid: data.sdid, scity: data.scity};
+	var result = api_twohouseListDel(params);
+	if(result.status==1){
+		list[findIndex].remove();
+	}
+	if($('#contrastList>li').length==1){
+		$('#contrastBtn').hide().siblings().show();
+	}
+	//重新刷新对比列表
+	appendContrastHtml();
+	//刷新状态
+	loginAfterRefresh();
+})
+
+//立即对比
+$('#contrastBtn').click(function(){
+	location.href="http://www.shyj.cn/#/contrast";
+})
+
+
+
+// console.log('------------------------------------------------加入收藏操作-----------------------------------------------------------')
+
+//收藏房源
+$('#addCollection').click(function(e){
+	if ($(this).text() == '已收藏') {
+		//取消收藏
+		var params = {scity: info.scity, sdid: info.sdid};
+		var result = api_cancelCollection1(params);
+		//取消收藏成功
+		if(result.status == '1'){		
+			$(this).text('收藏房源');	
+			return layer.msg('取消收藏');
+		}else{
+			var msg = result.msg;
+			return layer.msg(msg);
+		}	
+	} else {
+		//加入收藏(二手房)
+		var params = {scity: info.scity, sdid: info.sdid};
+		var result = api_addCollection1(params);
+		//收藏成功
+		if(result.status == '1'){		
+			$(this).text('已收藏');	
+			return layer.msg('收藏成功');
+		}else{
+			var msg = result.msg||'收藏失败';
+			return layer.msg(msg);
+		}		
+	} 
+})
+
+
+
+
+
+
+//监听分享移入移出
+$('.share,.myShare').mouseover(function(){
+	$('.myShare').show();
+}).mouseout(function(){
+	$('.myShare').hide();
+})
+
+//监听地图点击
+$('.mapQuery').click(function(){
+    location.href="http://www.shyj.cn/#/mapSearch?houseType=11";
+})
+
+//监听搜索框点击
+$('.search>input[type="submit"]').click(function(){
+	location.href="http://localhost:7031/custAppApi/house_c/twohouse?scity=beihai";
+})
+
